b796aff3bccb01fa5e2b203c754c5475981e3ffc
Rafiul <rafiul13062001@gmail.com>
2023-04-28 14:48:00 +0600

    Portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { RiFacebookCircleFill, RiLinkedinBoxFill, RiInstagramFill } from 'react-icons/ri'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_85jxg9s', 'template_xzip2bt', form.current, 'DpdF5Ml6upSF5O-1u')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+    };
+
     return (
         <div id='contact' className='px-3 w-full flex flex-col gap-8 pt-24'>
             <section className='flex flex-col md:flex-row gap-8'>
@@ -17,10 +33,10 @@ const Contact = () => {
                 </div>
 
                 <div className='flex-1'>
-                    <form className='w-full sm:px-16 md:px-0 flex flex-col gap-6'>
-                        <input className='border-solid border-blue border-2 rounded-md px-3 py-1' type="text" placeholder='Enter your name' />
-                        <input className='border-solid border-blue border-2 rounded-md px-3 py-1' type="email" name="" id="" placeholder='Enter your email' />
-                        <textarea className='border-solid border-blue border-2 rounded-md resize-none px-3 py-2' name="" id="" cols="30" rows="5" placeholder='Enter your message'></textarea>
+                    <form ref={form} onSubmit={sendEmail} className='w-full sm:px-16 md:px-0 flex flex-col gap-6'>
+                        <input className='border-solid border-blue border-2 rounded-md px-3 py-1' type="text" placeholder='Enter your name' name='user_name' required />
+                        <input className='border-solid border-blue border-2 rounded-md px-3 py-1' type="email" name="user_email" placeholder='Enter your email' required />
+                        <textarea className='border-solid border-blue border-2 rounded-md resize-none px-3 py-2' name="message" cols="30" rows="5" placeholder='Enter your message'></textarea>
                         <input className='w-full md:w-fit bg-blue text-white px-8 py-2 rounded-md hover:bg-pink-700 cursor-pointer' type="submit" value="Submit" />
                     </form>
                 </div>
